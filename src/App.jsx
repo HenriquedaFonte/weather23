40642fef3238c56ee09f3eb04667cdd7f2db6b8a
Henrique da Fonte <henriquegdafonte@gmail.com>
2023-03-21 20:35:23 -0400

    Feat: Start to conect API

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+
 import './App.css'
 import Pin from './assets/pin.svg'
 import Rain from './assets/rain.svg'
@@ -15,16 +18,47 @@ import PartlyCloudy from './assets/partlyCloudy.svg'
 import Day from "./components/Day/Day";
 
 function App() {
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState('Recife')
+  const [showInput, setShowInput] = useState(false);
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=bf71fe4e34456d42012ad42c1757b5f3`
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      setShowInput(false);
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })      
+    }
+  }
+
+  console.log(location);
+  console.log(data.name);
+
   return (
     <div className="app">   
         <section className="weather-now">
           <div className="location">
             <img src={Pin} alt="Location Pin icon" />
-            <strong>Recife, PE</strong>
+            <div>
+              {showInput ? (
+                <input
+                  className='searchLocation'
+                  type="text"
+                  value={location}
+                  onChange={(event) => setLocation(event.target.value)}
+                  onKeyDown={searchLocation}
+                />
+              ) : (
+                <strong onClick={() => setShowInput(true)}>{data.name ? data.name : 'Recife'}</strong>
+              )}
+          </div>
           </div>
           <div className="temperature">
             <div className="number">
-              20
+              {data.main ? data.main.temp +'°C' : null}
               <div className="tempminmax">
                 25° <span>15°</span>
               </div>
